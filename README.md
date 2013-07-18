f3b9c19fa12877e5f5bdf0ad9b9b2c521a7aff0c
pocesar <gahgneh+github@gmail.com>
2013-07-18 04:15:24 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
+[![Build Status](https://travis-ci.org/pocesar/node-stratum.png?branch=master)](https://travis-ci.org/pocesar/node-stratum)
+
 node-stratum
 ============
 
-Stratum protocol server using Express
+Exposes a function to enable Stratum protocol usage on Node.js and subscribe for events using EventEmitter
+
+## Install
+
+```bash
+npm install stratum
+```
+
+## Usage
+
+```js
+var
+    Stratum = require('stratum'),
+    server = Stratum.create();
+
+server.on('subscribe', function(){
+});
+
+server.listen(3333);
+```
+
+## Do you like it? Wanna support the development?
+
+```bash
+npm star stratum
+```
+
+`BTC: 1PskTzQjmzqB2boz67AXsv4C5YNWN4xmhu`
+`LTC: LW2kXiquqDu3GfpfBX2xNTSgjVmBPu6g3z`
+`PPC: PNKZEkDf9eBSNebu2CcxHaGuma6wHuZEBh`
---
+++ test/tests.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var
+  expect = require('expect.js'),
+  stratum = require('../lib/index.js'),
+  server;
+
+module.exports = {
+  beforeEach: function(){
+    server = stratum.create();
+  },
+  afterEach: function(){
+    var tcp = (server, server.server());
+    if (tcp && tcp.close) {
+      tcp.close();
+    }
+    server = null;
+  },
+  Stratum: {
+    testExceptionOnListening: function(){
+      expect(server.listen).to.throwError();
+    },
+    testEventEmitter: function(done){
+      server.listen(3002, function(){
+        expect(stratum.listenerCount).to.be.ok();
+        expect(server.on).to.be.a('function');
+        expect(server.emit).to.be.a('function');
+        done();
+      });
+    },
+    testMethods: function(done){
+      server.listen(3004, function(){
+        var tcp = server.server();
+        expect(tcp.hasMethod('mining.subscribe')).to.equal(true);
+        expect(tcp.hasMethod('mining.authorize')).to.equal(true);
+        expect(tcp.hasMethod('mining.submit')).to.equal(true);
+        done();
+      });
+    }
+  }
+};
